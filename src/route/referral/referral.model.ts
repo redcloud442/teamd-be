3bff9cf82c257deaf13427169e8dd249aad1da58
cloudred <cloudred442@gmail.com>
2025-02-15 16:40:35 +0800

    enwest commit

--- src/route/referral/referral.model.ts
+++ src/route/referral/referral.model.ts
@@ -80,7 +80,7 @@ export const referralDirectModelPost = async (params: {
         FROM alliance_schema.alliance_member_table m
         JOIN user_schema.user_table u ON u.user_id = m.alliance_member_user_id
         JOIN packages_schema.package_ally_bounty_log pa ON pa.package_ally_bounty_from = m.alliance_member_id
-        WHERE pa.package_ally_bounty_member_id = ${teamMemberProfile.alliance_member_id}::uuid
+        WHERE pa.package_ally_bounty_member_id = ${teamMemberProfile.alliance_member_id}::uuid AND pa.package_ally_bounty_type = 'DIRECT'
           ${searchCondition}
         GROUP BY u.user_first_name, u.user_last_name, u.user_username, pa.package_ally_bounty_log_date_created
     ) AS subquery;
