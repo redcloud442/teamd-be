a75f34554f525b077fd07a5e82629ce8e4cee58d
cloudred <cloudred442@gmail.com>
2025-02-27 09:38:52 +0800

    newest commit

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,30 +1,23 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-// Initialize Redis once
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL || "https://default.redis.url'",
   token: process.env.UPSTASH_REDIS_REST_TOKEN || "'default-redis-token'",
 });
 
-// Initialize Ratelimit once and reuse it
-
-let denyList: Set<string> = new Set();
-let lastDenyListFetch = 0; // Timestamp of last fetch
-
 /**
- * Rate Limit Function with Country Blocking
+ * Rate Limit Function with Dynamic Time Window
  * @param {string} identifier - Unique user key (IP, User ID, etc.)
- * @param {string} country - Country of the incoming request
- * @returns {boolean} - `true` if request is allowed, `false` if blocked
+ * @param {number} maxRequests - Maximum allowed requests
+ * @param {string} timeWindow - Time duration (e.g., "10 s", "1 m", "5 m", "1 h")
+ * @returns {boolean} - `true` if request is allowed, `false` if rate limit exceeded
  */
 export async function rateLimit(
   identifier: string,
   maxRequests: number,
-  timeWindow: "10s" | "1m" | "1h" | "1d"
+  timeWindow: "10s" | "1m" | "5m" | "1h"
 ) {
-  const now = Date.now();
-
   const ratelimit = new Ratelimit({
     redis: redis as any,
     limiter: Ratelimit.slidingWindow(maxRequests, `${timeWindow}`),
@@ -32,20 +25,10 @@ export async function rateLimit(
     analytics: true,
   });
 
-  if (now - lastDenyListFetch > 600000) {
-    const countries = await redis.smembers(
-      "@upstash/ratelimit:denyList:country"
-    );
-
-    denyList = new Set(countries);
-    lastDenyListFetch = now;
-  }
-
-  if (denyList.has(identifier)) {
-    return false;
-  }
   const { success, pending } = await ratelimit.limit(identifier, {
-    country: Array.from(denyList).join(","),
+    ip: "ip-address",
+    userAgent: "user-agent",
+    country: "country",
   });
 
   await pending;
