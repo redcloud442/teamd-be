0e2822412526c3a68620e8f62626b71880a4acb6
redcloud442 <cloudred442@gmail.com>
2025-03-07 11:38:41 +0800

    newest commit

--- src/route/user/user.middleware.ts
+++ src/route/user/user.middleware.ts
@@ -498,7 +498,7 @@ export const userListReinvestedMiddleware = async (c: Context, next: Next) => {
 export const userTreeMiddleware = async (c: Context, next: Next) => {
   const user = c.get("user");
 
-  const response = await protectionAdmin(user.id, prisma);
+  const response = await protectionAccountingAdmin(user.id, prisma);
 
   if (response instanceof Response) {
     return response;
