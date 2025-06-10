712f24bc9a254f5f0a89cb5dfb885e0a0056ef46
cloudred <cloudred442@gmail.com>
2025-06-10 09:13:53 +0800

    initial commit

--- src/route/deposit/deposit.model.ts
+++ src/route/deposit/deposit.model.ts
@@ -408,7 +408,7 @@ export const depositListPostModel = async (
     );
     commonConditions.push(
       Prisma.raw(
-        `t.company_deposit_request_date_updated::timestamptz at time zone 'Asia/Manila' BETWEEN '${startDate}'::timestamptz AND '${endDate}'::timestamptz`
+        `t.company_deposit_request_date::timestamptz at time zone 'Asia/Manila' BETWEEN '${startDate}'::timestamptz AND '${endDate}'::timestamptz`
       )
     );
   }
